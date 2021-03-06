b6cace3cb1f99d70241ed748a89f9173853d5267
AnhNDT <anhnguyen.lk@gmail.com>
2021-03-06 15:13:24 +0700

    remove unusual resources

--- lib/common/constants/graphic_constants.dart
+++ lib/common/constants/graphic_constants.dart
@@ -1,27 +1,5 @@
 class GraphicConstants {
-  static const loginBackground = 'res/graphics/img_login_bg.jpg';
 }
 
 class IconConstants {
-  static const icHome = 'res/icons/ic_home.svg';
-  static const icGift = 'res/icons/ic_gift.svg';
-  static const icStores = 'res/icons/ic_stores.svg';
-  static const icMore = 'res/icons/ic_more.svg';
-
-  static const icCar = 'res/icons/ic_car.svg';
-  static const icUser = 'res/icons/ic_user.svg';
-  static const icMembership = 'res/icons/ic_membership.svg';
-  static const icBug = 'res/icons/ic_bug.svg';
-  static const icArrowRight = 'res/icons/ic_arrow_right.svg';
-  static const icOrderHistory = 'res/icons/ic_order_history.svg';
-  static const icPolicy = 'res/icons/ic_policy.svg';
-  static const icHelp = 'res/icons/ic_help.svg';
-  static const icSignOut = 'res/icons/ic_sign_out.svg';
-  static const icShareReferralCode = 'res/icons/ic_share_referral_code.svg';
-  static const icVipMembership = 'res/icons/ic_member.svg';
-  static const icCoupon = 'res/icons/ic_coupon.svg';
-  static const icBook = 'res/icons/ic_book_calendar.svg';
-  static const icAbout = 'res/icons/ic_about.svg';
-  static const icPhone = 'res/icons/ic_phone.svg';
-  static const icQrCode = 'res/icons/ic_scan_qr.svg';
 }
